5cc9b7360aaaf8ea60d424e23f7cb069d226ea3e
Asadbek <asadbek.rahimov@gmail.com>
2026-02-01 23:49:10 +0500

    fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->validateCsrfTokens(except: [
+            'telegram/webhook',
+        ]);
+        
         $middleware->alias([
             'telegram.webhook' => \App\Http\Middleware\VerifyTelegramWebhook::class,
         ]);
